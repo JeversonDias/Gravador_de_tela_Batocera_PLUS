ee4ee4db23436010306e0225c78d3fb58a247fad
JeversonDias <batocera.firma@gtmail.com>
2023-10-11 12:27:48 -0300

    Pronto para os primeiros testes

---
+++ Instalador.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+###############################################################################################################
+###############################################################################################################
+#Código escrito por Jeverson Dias Da Silva & Cauã Batista Dias Da Silva... @JCGAMESCLASSICOS \YOUTUBE
+#Sistema criado para gravar a tela do batocera.PLUS ou batocera Linux
+#Auto instala ao executar este arquivo!
+###############################################################################################################
+###############################################################################################################
+unzip ++.zip
+cd rec++ 
+./instalador
+rm instalador
+cd .. 
+rm rec.zip
+rm Readmi.txt
+rmdir rec++
+rm rec.zip
+rm ./Instalador.sh
+batocera-save-overlay
---
+++ Readmi.md
@@ -0,0 +1,10 @@
+Olá, para instalar o programa basta executar o Instalador.sh e aguardar...
+
+Leve menos de um minuto.
+
+É super leve todo feito em Shell Script. 
+
+Caso tenha dúvidas procura pelos nossos vídeos no youtube no nosso canal, JC GAMES CLÁSSICOS !
+
+# Caso queira contribuir com os nossos projetos aqui está a nosa chave PIX (41)992265547 (Jeverson Dias da Silva)
+E caso tenha algum projeto em específico (MULTI JOGOS) para desenvolver entra em contato pelo Yuotube  ...
